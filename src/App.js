3b1c75e69d9b46219bc9221a05bf8f5b113bb315
kr3t2115 <szymon.baranowicz@teb.edu.pl>
2022-09-23 12:05:54 +0200

    Add Paginator

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import Pagination from "./components/Pagination";
+import "./App.css";
+import List from "./components/List";
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [imagePerPage, setImagePerPage] = useState(4);
+  const [allData, setAllData] = useState([]);
+
+  const eventHandler = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const sendRequest = async (params) => {
+    let url = `https://pixabay.com/api/?key=30072676-d250de6e9ee4620711444e58c&q=${params}&image_type=photo&pretty=true`;
+
+    try {
+      const request = await fetch(url);
+
+      if (request.status !== 200) {
+        throw new Error(request.statusText);
+      }
+      const parsedData = await request.json();
+
+      setAllData([]);
+
+      await parsedData.hits.forEach((element) => {
+        setAllData((arrayItem) => [
+          ...arrayItem,
+          { image: element.previewURL, alt: element.tags },
+        ]);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    document.title = "You searched: " + query;
+
+    const loading = document.getElementById("loading");
+
+    loading.classList.add("active");
+
+    const timeout = setTimeout(() => {
+      if (query === "") {
+        loading.classList.remove("active");
+        return;
+      } else {
+        loading.classList.remove("active");
+
+        sendRequest(query);
+      }
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [query]);
+
+  useEffect(() => {}, [allData]);
+
+  useEffect(() => {
+    if (window.innerWidth > 700 && window.innerWidth < 1200) {
+      setImagePerPage(3);
+    }
+  }, [imagePerPage]);
+
+  const lastPostIndex = currentPage * imagePerPage;
+  const firstPostIndex = lastPostIndex - imagePerPage;
+  const currentPosts = allData.slice(firstPostIndex, lastPostIndex);
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Search image from pixabay</h2>
+        <input
+          style={{ margin: "20px" }}
+          onChange={(event) => eventHandler(event)}
+        ></input>
+        <br></br>
+        <div className="loader-circle-6" id="loading"></div>
+      </div>
+
+      <List data={currentPosts}></List>
+
+      <Pagination
+        dataLength={allData.length}
+        imagePerPage={imagePerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      ></Pagination>
+    </div>
+  );
+}
+
+export default App;
